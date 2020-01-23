bcecca1b29a1f1ea2bdc3d93820977963102f5e7
Foxfail <foxfail@mail.ru>
2020-01-23 18:40:02 +0300

    Простой пример StreamAPI

--- src/com/foxfail/Main.java
+++ src/com/foxfail/Main.java
@@ -1,8 +1,11 @@
 package com.foxfail;
 
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // создаем стрим интов, отбираем из него три числа которые больше 60 и выводим их на экран
+        IntStream.of(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120).filter(x -> x > 60).limit(3).forEach(System.out::println);
     }
 }
